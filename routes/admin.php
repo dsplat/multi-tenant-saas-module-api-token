3a8c4873c70fcf10fd617ce310ec97a9e284f64d
luoyueliang <luoyueliang@gmail.com>
2026-07-12 07:29:38 +0800

    sync: feat: 全部模块 admin/tenant 路由 (Pint 修复)

---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use MultiTenantSaas\Modules\ApiToken\Http\Controllers\TenantTokenController;
+
+Route::prefix('admin/api-tokens')->group(function () {
+    Route::get('/', [TenantTokenController::class, 'adminIndex']);
+    Route::delete('/{tenantId}/{tokenId}', [TenantTokenController::class, 'adminDestroy']);
+});
